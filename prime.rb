d58c5bc62af9f2d99a0d8a4c83bb115c2791023b
Janice Darikho <janicedarikho@gmail.com>
2018-05-20 16:15:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(int)
-  if int % 2 == 0 || int % 3 == 0 || int % 5 == 0 || int % 7 == 0 || int % 9 == 0 || int % 11 == 0 || int % 13  == 0
+  if 
     false
   else
     true
